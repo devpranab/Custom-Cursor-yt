0b0560ac985182b59b33427e20c4b795263d54f7
Pranab Sarkar <86111675+devpranab@users.noreply.github.com>
2022-02-18 14:31:07 +0530

    Update script.js
    move cursor add script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //Select element
+
+let moveCursor = document.querySelector(".cursor");
+
+window.addEventListener("mousemove", movingFunction);
+
+function movingFunction(e){
+   //console.log(e);
+   moveCursor.style.top = e.pageY + "px";
+   moveCursor.style.left = e.pageX + "px";
+
+}
